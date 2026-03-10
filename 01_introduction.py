6134188963d20c9c94221ffbe43129ba545cdd5e
maruf-git <munna.csehstu@gmail.com>
2026-03-10 20:05:11 +0600

    python introduction

---
+++ 01_introduction.py
@@ -0,0 +1,10 @@
+# Python runs on an interpreter system, meaning that code can be executed as soon as it is written. This means that prototyping can be very quick.
+# Python can be treated in a procedural way, an object-oriented way or a functional way.
+
+# Python Syntax compared to other programming languages
+# Python was designed for readability, and has some similarities to the English language with influence from mathematics.
+# Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
+# Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose.
+
+import sys
+print(sys.version)
