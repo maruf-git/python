33e5e110bf413199cdbf36d8c483011f12215ac2
maruf-git <munna.csehstu@gmail.com>
2026-03-10 20:34:07 +0600

    print in python

---
+++ 03_print.py
@@ -0,0 +1,14 @@
+# plain text print with newline
+
+print("Hello World")
+print("How are you all?")
+
+# print without a new line
+
+print("Welcome to our home!!", end= " ")
+print("My dear friend Rifat!!")
+
+# Mix Text and Numbers
+# You can combine text and numbers in one output by separating them with a comma:
+
+print("I am", 35,"years old.")
