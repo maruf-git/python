7a03b80221d8c3ba8af740e7ee90082533bbdc50
maruf-git <munna.csehstu@gmail.com>
2026-03-10 20:15:00 +0600

    python syntax

---
+++ 02_python_syntax.py
@@ -0,0 +1,24 @@
+# printing
+print("Hello World!")
+
+# indentation are important in python
+# default practice is 4 spaces
+if 5>2:
+    print("5 is greater than 2")
+    
+# python variables
+# Python has no command for declaring a variable.
+
+value = 5
+name = "maruf"
+print(name,value)
+
+# comments
+
+# statement
+# In Python, a statement usually ends when the line ends. You do not need to use a semicolon (;) like in many other programming languages (for example, Java or C).
+
+# Semicolons (Optional, Rarely Used)
+# Semicolons are optional in Python. You can write multiple statements on one line by separating them with ; but this is rarely used because it makes it hard to read:
+
+print("Hello"); print("How are you?"); print("Bye bye!");
