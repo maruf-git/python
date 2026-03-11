1bc47ea435ece75aa1a29e7cd46955d2e312c2d6
maruf-git <munna.csehstu@gmail.com>
2026-03-11 23:45:40 +0600

    strings

---
+++ 07_casting.py
@@ -0,0 +1,4 @@
+# Specify a Variable Type
+# There may be times when you want to specify a type on to a variable. This can be done with casting. Python is an object-orientated language, and as such it uses classes to define data types, including its primitive types.
+
+# Casting in python is therefore done using constructor functions:
---
+++ 08_python_strings.py
@@ -0,0 +1,11 @@
+# Strings
+# Strings in python are surrounded by either single quotation marks, or double quotation marks.
+
+
+# Quotes Inside Quotes
+# You can use quotes inside a string, as long as they don't match the quotes surrounding the string:
+
+# Example
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
