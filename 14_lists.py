b16e56015218277d6534a51ea17483988bd6e60d
maruf-git <munna.csehstu@gmail.com>
2026-03-25 23:56:47 +0600

    python list

---
+++ 14_lists.py
@@ -0,0 +1,7 @@
+# list are changeable in python
+# The list is changeable, meaning that we can change, add, and remove items in a list after it has been created.
+
+thislist = ["banana", "apple", "litchi"];
+print(thislist)
+
+print(len(thislist))
