9171311a388383ef23ce2b199311c30f00c693d6
maruf-git <munna.csehstu@gmail.com>
2026-03-27 00:11:24 +0600

    try and except in python

---
+++ 19_try_except.py
@@ -0,0 +1,19 @@
+# print(x)
+# try:
+#     print(x)
+# except:
+#     print("Something went wrong")
+# finally:
+#     print("Thing that I will do always")
+
+# x = 50
+try:
+    print(x)
+except:
+    print("Something went wrong")
+else:
+    print("If no error found then this will also run")
+finally:
+    print("This will always run")
+
+print("Outside try and except")
