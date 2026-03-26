41b5bf628a2a16c84d8e99bff29093c2cd0814bb
maruf-git <munna.csehstu@gmail.com>
2026-03-26 19:57:14 +0600

    python loops basic

---
+++ 15_loops.py
@@ -0,0 +1,18 @@
+# for loop in python
+for i in range(1,11): #(start,end-1,update)
+    print(i,"hello world!")
+print("*************************")
+
+# print 10 to 2 using for loop
+for i in range(10,0,-2):
+    print(f"{i}: hello world!")
+print(f"type of range is: {type(range)}")
+
+# while loop in python is same as c and c++
+# note python does not have increment and decrement operator
+print("While loop in python:")
+i = 5;
+while(i<=10):
+    print(f"{i}: hello world!")
+    i+=1
+    
