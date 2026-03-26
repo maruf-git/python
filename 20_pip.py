c37c6c8348efe98755fff607250a93a53b084294
maruf-git <munna.csehstu@gmail.com>
2026-03-27 00:18:08 +0600

    pip package manager

---
+++ 20_pip.py
@@ -0,0 +1,11 @@
+# What is PIP?
+# PIP is a package manager for Python packages, or modules if you like.
+import camelcase
+
+c = camelcase.CamelCase()
+txt = "hello world"
+print(c.hump(txt))
+
+# pip install camelcase
+# pip uninstall camelcase
+# pip list -> to know the packages installed in the system
